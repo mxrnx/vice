c9ac66a27e8c8530fdd4edb712e329dff93b5067
knarka <knarka@users.noreply.github.com>
2018-05-30 16:53:17 +0200

    implement basic ui

--- src/vice.rb
+++ src/vice.rb
@@ -1,8 +1,11 @@
+require 'curses'
+
 module Vice
 	class Vice
 		attr_accessor :buffers
 		attr_accessor :mode
 		attr_accessor :cursor
+		attr_accessor :currentbuffer
 
 		def initialize
 			@mode = :command
@@ -11,11 +14,23 @@ module Vice
 		end
 
 		def start
+			blitter = Blitter.new
+
 			# for now: create a single buffer
 			@buffers.push Buffer.new nil, nil
+			@currentbuffer = 0
+
+			Curses.init_screen
+			Curses.noecho
+			window = Curses.stdscr
 
 			loop do
+				blitter.drawbuffer(@mode, window, @buffers[@currentbuffer])
+				key = window.getch
+				@parser.parsekeypress self, @currentbuffer, key
 			end
+
+			Curses.close_screen
 		end
 	end
 end
@@ -23,3 +38,4 @@ end
 require_relative "vice/buffer"
 require_relative "vice/cursor"
 require_relative "vice/parser"
+require_relative "vice/blitter"
